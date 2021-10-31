a8cbafcf8aaa1d368b406ebdd2c43f076771e400
mfarzamalam <mfarzamalam@gmail.com>
2021-10-31 16:53:19 +0500

    added models in courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import tree
+from stripe_monthly.models import Memebership
 
-# Create your models here.
+
+
+class Course(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    allowed_membership = models.ManyToManyField(Memebership)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Lesson(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    position = models.IntegerField()
+    video_url = models.CharField(max_length=100)
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
--- django_stripe/settings.py
+++ django_stripe/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'stripe_monthly',
+    'courses',
 ]
 
 MIDDLEWARE = [
