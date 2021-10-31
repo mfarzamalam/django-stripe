19e857b71561dd0265c57a1093b417a0f236e650
mfarzamalam <mfarzamalam@gmail.com>
2021-10-31 15:11:34 +0500

    added models for monthly memberships

--- stripe_monthly/admin.py
+++ stripe_monthly/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from stripe_monthly.models import Memebership, UserMembership, Subscription 
 
-# Register your models here.
+
+
+admin.site.register(Memebership)
+admin.site.register(UserMembership)
+admin.site.register(Subscription)
--- stripe_monthly/urls.py
+++ stripe_monthly/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
+from stripe_monthly.views import HomeView
+
 
 urlpatterns = [
-    # path('')
-]
+    path('', HomeView.as_view(), name='s_home'),
+]  + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
